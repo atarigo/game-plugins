5c59087e4c01ccb932b4cba403bb271b0ac99b49
wenke.lai <wenke.studio@gmail.com>
2025-11-03 12:04:57 +0800

    feat: add game node

--- plugins/core/__init__.py
+++ plugins/core/__init__.py
@@ -1,5 +1,7 @@
 from .event_manager import EventManager
+from .game_node import GameNode
 
 __all__ = [
     "EventManager",
+    "GameNode",
 ]
---
+++ plugins/core/game_node.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class GameNode:
+    def handle_event(self, event: pygame.event.Event):
+        pass
+
+    def update(self, dt: float):
+        pass
+
+    def render(self, screen: pygame.Surface):
+        pass
